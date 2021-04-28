67364d9e7f96f76431e19ffd8d465e00ccafd78d
ketanb56up <ketanb56up@gmail.com>
2021-04-28 18:47:42 +0530

    temp

---
+++ assets/js/Componet/Layouts/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Header = () => {
+    return (
+        <Fragment>
+            <h1>Hello Header</h1>
+        </Fragment>
+    )
+}
+
+export default Header
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-render() {
-        return (
-            <div>
-                This is my React app!
-            </div>);
-    }
+import Login from './Componet/Login/Login';
+import Register from './Componet/Register/Register';
+import Header from './Componet/Layouts/Header/Header';
+import Home from './Componet/Home/Home';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <div className="container container-fluid">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    )
 }
+
+export default App
+
 ReactDOM.render(<App />, document.getElementById('react-app'));

06211040e30922de9097b7f44e4bc5fcf30ce5e0
ketanb56up <ketanb56up@gmail.com>
2021-04-28 11:41:42 +0530

    initial setup

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+render() {
+        return (
+            <div>
+                This is my React app wohoooo yeee oooo!
+            </div>);
+    }
+}
+ReactDOM.render(<App />, document.getElementById('react-app'));
---
+++ page/urls.py
@@ -0,0 +1,6 @@
+# page/urls.py
+from django.urls import path
+from .views import index
+urlpatterns = [
+    path('', index, name='home'),
+]
